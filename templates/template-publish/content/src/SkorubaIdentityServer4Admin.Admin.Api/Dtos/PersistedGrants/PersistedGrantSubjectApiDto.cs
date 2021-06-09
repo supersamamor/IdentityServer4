02ed459123ac3f852ab3921128a83a5491a9a33c
Samuel N. Amor IV <supersam.amor@gmail.com>
2021-06-09 20:00:55 +0800

    Initial Commit

---
+++ templates/template-publish/content/src/SkorubaIdentityServer4Admin.Admin.Api/Dtos/PersistedGrants/PersistedGrantSubjectApiDto.cs
@@ -0,0 +1,15 @@
+﻿namespace SkorubaIdentityServer4Admin.Admin.Api.Dtos.PersistedGrants
+{
+    public class PersistedGrantSubjectApiDto
+    {
+        public string SubjectId { get; set; }
+        public string SubjectName { get; set; }
+    }
+}
+
+
+
+
+
+
+
