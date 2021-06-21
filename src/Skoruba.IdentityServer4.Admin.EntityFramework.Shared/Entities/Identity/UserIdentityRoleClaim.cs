d001114cbfbe28eebe8f87f2070d3a792110a5ad
Samuel N. Amor IV <supersam.amor@gmail.com>
2021-06-21 12:23:00 +0800

    Removed unused fields from role claims table

--- src/Skoruba.IdentityServer4.Admin.EntityFramework.Shared/Entities/Identity/UserIdentityRoleClaim.cs
+++ src/Skoruba.IdentityServer4.Admin.EntityFramework.Shared/Entities/Identity/UserIdentityRoleClaim.cs
@@ -5,9 +5,5 @@ namespace Skoruba.IdentityServer4.Admin.EntityFramework.Shared.Entities.Identity
 {
     public class UserIdentityRoleClaim : IdentityRoleClaim<string>
     {
-        public int? ApplicationId { get; set; }
-        public string Permission { get; set; }
-        public string Module { get; set; }
-        public Application Application { get; set; }
     }
 }
---
+++ src/Skoruba.IdentityServer4.Admin.EntityFramework.SqlServer/Migrations/Identity/20210621041121_RemoveCustomFieldsOnRoleClaimsTable.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Skoruba.IdentityServer4.Admin.EntityFramework.SqlServer.Migrations.Identity
+{
+    public partial class RemoveCustomFieldsOnRoleClaimsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RoleClaims_Applications_ApplicationId",
+                table: "RoleClaims");
+
+            migrationBuilder.DropIndex(
+                name: "IX_RoleClaims_ApplicationId",
+                table: "RoleClaims");
+
+            migrationBuilder.DropColumn(
+                name: "ApplicationId",
+                table: "RoleClaims");
+
+            migrationBuilder.DropColumn(
+                name: "Module",
+                table: "RoleClaims");
+
+            migrationBuilder.DropColumn(
+                name: "Permission",
+                table: "RoleClaims");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ApplicationId",
+                table: "RoleClaims",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Module",
+                table: "RoleClaims",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Permission",
+                table: "RoleClaims",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RoleClaims_ApplicationId",
+                table: "RoleClaims",
+                column: "ApplicationId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RoleClaims_Applications_ApplicationId",
+                table: "RoleClaims",
+                column: "ApplicationId",
+                principalTable: "Applications",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
